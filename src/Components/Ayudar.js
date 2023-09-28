09725cd31ce6b0ceeac55df04f09d43221620b5e
Irian Cortés <icortesdev@proton.me>
2023-09-28 12:25:58 +0200

    compnt ayudar

--- src/Components/Ayudar.js
+++ src/Components/Ayudar.js
@@ -2,6 +2,96 @@ import React from 'react'
 
 export const Ayudar = () => {
     return (
-        <div>Ayudar</div>
-    )
+        <div>
+            <div className="card-category-2">
+                <span className="category-name">¿Como ayudar?</span> <br /><br />
+                <div className="card-list-container">
+                    <ul className="card-list">
+                        <li>
+
+                            <div class="basic-card basic-card-aqua">
+                                <div class="card-content">
+                                    <span class="card-title"><strong>Donación via cuenta bancaria</strong></span>
+                                    <p class="card-text">
+
+                                        Tu aportación es muy valiosa para que podamos seguir adelante con nuestro proyecto, rescatando perros del abandono y el maltrato.
+
+
+
+
+
+                                        Si quieres ayudarnos  mediante transferencia bancaria:
+                                        <br />
+                                        <strong> 3159 0006 5127 6628 9728</strong>
+
+
+
+                                    </p>
+                                </div>
+
+                                <div class="card-link">
+                                    <a href="#" title="Read Full"><span>Saber más</span></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="basic-card basic-card-aqua">
+                                <div class="card-content">
+                                    <span class="card-title"><strong>Donación via Bizum</strong></span>
+                                    <p class="card-text">
+                                        Accede a Bizum desde la app de tu banco, busca la opción «Enviar y Recibir dinero con bizum«,
+                                        <br />
+                                        teclea la cantidad de dinero que quieras enviar y como destinatario, este número de teléfono:
+                                        <br /> <strong> +34 670373930.</strong>
+                                    </p>
+                                </div>
+
+                                <div class="card-link">
+                                    <a href="#" title="Read Full"><span>Saber más</span></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="basic-card basic-card-aqua">
+                                <div class="card-content">
+                                    <span class="card-title"><strong>Unete a Teaming</strong> </span>
+                                    <p class="card-text">
+
+                                        Con Teaming puedes donar un euro al mes con solo unirte al grupo de Galgos Felices Fundación.
+
+                                        Solo tienes que registrarte en Teaming, si no lo estás y unirte a nuestro grupo:
+                                        <br />
+                                        <br />
+                                        <a href='https://www.teaming.net/group/list?q=galgos%20felices%20fundacion'>GRUPOR DE TEAMING</a>
+                                    </p>
+                                </div>
+
+                                <div class="card-link">
+                                    <a href="#" title="Read Full"><span>Saber más</span></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="basic-card basic-card-aqua">
+                                <div class="card-content">
+                                    <span class="card-title"><strong>Apadrina</strong></span>
+                                    <p class="card-text">
+                                        El apadrinar un animal es mucho más de lo que te imaginas, creas un vínculo especial con uno de nuestros peludos, formas parte de su vida y aunque estés lejos te conviertes en su familia mientras esperan a encontrar un hogar.
+
+
+                                        Tú decides cuanto quieres aportar.
+                                    </p>
+                                </div>
+
+                                <div class="card-link">
+                                    <a href="#" title="Read Full"><span>Saber más</span></a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <br />
+        </div>
+    );
 }
