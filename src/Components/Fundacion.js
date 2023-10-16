561f80c75b41f5dce954c3acc07168e5c9bac06f
Irian Cortés <icortesdev@proton.me>
2023-10-16 19:09:31 +0200

    Fundacion component update

--- src/Components/Fundacion.js
+++ src/Components/Fundacion.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
+
 export const Fundacion = () => {
     return (
-        <div>Fundacion</div>
+        <div className='titulo'><h2>Conócenos</h2>
+            <div className='presentacion'>
+                <p>Hola amigos:</p>
+                <p> Yo soy <strong>Pepa</strong>, y como podéis ver, los perros son mi pasión.
+                    En 2021 y tras un año de retraso por la pandemia, por fin, pude realizar mi gran sueño de crear un sitio donde poder acoger a muchos peludos que necesitan nuestra ayuda. Cuidarlos y recuperarlos tanto física como anímicamente y buscarles a cada uno su familia ideal, esa familia que les dará una vida digna y todo el cariño y amor que se merecen todos y cada uno de los peluditos.</p>
+
+                <p>Esta labor requiere de mucho trabajo y muchas horas de dedicación, tantas que si no fuese porque tengo junto a mi un pequeño gran equipo de voluntarios, no podría realizarse.</p>
+                <img src='/Gabo web.jpg' alt='gabo' style={{ width: '500px', height: 'auto ' }}></img> <img src='/4a67516f-abec-4472-bab6-d1b618e32460.jpg' alt='imgfund1' style={{ width: '473px', height: 'auto' }}></img>
+                <p>Gabo, mi primer galgo y mi gran inspiración para crear la  fundación, hogar de paso de muchos peludos que por circunstancias de la vida son abandonados, maltratados o humillados en toda su inocencia.
+
+                    A todos ellos y por todos ellos, les dedico cada día todo mi esfuerzo y mi ilusión para que consigan tener un hogar en paz y amor, como se merecen.</p>
+            </div>
+        </div>
+
+
     )
 }
